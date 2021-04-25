96212a4e3c3dd08d468128452d34345f5e07f14a
Caleb Adewole <caleberioluwa@gmail.com>
2021-04-25 19:22:34 +0100

    remove node_modules

--- index.js
+++ index.js
@@ -3,47 +3,41 @@ var userModel = require("./models/model");
 const { keyword } = require("./search/tags");
 const producer = require("./helpers/kafka-producer");
 
-
 var tw_api = new TwitterStreamInterface(
-        "dz9XC0e99nHqlneDA1n4JiNsd",
-        "W2FuRyg3GDwrHLx6v1OMzMNwL3lL5lmkPLfVsGHtR4y91bR8su",
-        "3159540323-9IprnZmDCtgZ1xfQkGaVxNV5xJDyvDknY0nbdRE",
-        "cpHgAPsfz0jmBCybmfuu88E8JFZG7hgAnlLMha2Re3xkq"
+  "dz9XC0e99nHqlneDA1n4JiNsd",
+  "W2FuRyg3GDwrHLx6v1OMzMNwL3lL5lmkPLfVsGHtR4y91bR8su",
+  "3159540323-9IprnZmDCtgZ1xfQkGaVxNV5xJDyvDknY0nbdRE",
+  "cpHgAPsfz0jmBCybmfuu88E8JFZG7hgAnlLMha2Re3xkq"
 );
 
-tw_api.Twitter()
-        .stream("statuses/filter", { track: keyword })
-        .on("tweet", tweet => {
-                var temp = new userModel(
-                        tweet.user.id,
-                        tweet.source,
-                        tweet.retweeted,
-                        tweet.retweet_count,
-                        tweet.created_at,
-                        tweet.user.name,
-                        tweet.id_str,
-                        tweet.profile_image_url,
-                        tweet.followers_count,
-                        tweet.text
-                );
+tw_api
+  .Twitter()
+  .stream("statuses/filter", { track: keyword })
+  .on("tweet", (tweet) => {
+    var temp = new userModel(
+      tweet.user.id,
+      tweet.source,
+      tweet.retweeted,
+      tweet.retweet_count,
+      tweet.created_at,
+      tweet.user.name,
+      tweet.id_str,
+      tweet.profile_image_url,
+      tweet.followers_count,
+      tweet.text
+    );
+    // setting the host  and options
+    var kafkaHost = {
+      kafkaHost: "52.19.199.252:9092,52.19.199.252:9093,52.19.199.252:9094",
+    };
+    var kafka_production = producer(kafkaHost);
 
-                console.log(JSON.stringify(temp.objectify()))
-                // setting the host  and options
-                var kafkaHost = {
-                        kafkaHost:
-                                "52.19.199.252:9092,52.19.199.252:9093,52.19.199.252:9094"
-                };
-                var kafka_production = producer(kafkaHost);
+    const payload = [
+      {
+        topic: "caleb_twitter_stream_topic",
+        messages: JSON.stringify(temp.objectify()),
+      },
+    ];
 
-                const payload = [
-                        {
-                                topic: "caleb_twitter_stream_topic",
-                                messages: JSON.stringify(temp.objectify())
-                        }
-                ];
-
-                
-                kafka_production.sendPayload(payload);
-        });
-
-
+    kafka_production.sendPayload(payload);
+  });
--- models/model.js
+++ models/model.js
@@ -1,35 +1,42 @@
 class Model {
-        constructor(tweet_id,source,retweeted,retweeted_count,created_at,username,user_id,profile_img_url,followers,tweet){
-                this.source = source;
-                this.retweeted = retweeted;
-                this.retweeted_count = retweeted_count;
-                this.created_at = created_at;
-                this.username = username;
-                this.profile_img_url = profile_img_url;
-                this.followers = followers;
-                this.user_id = user_id;
-                this.tweet_id = tweet_id;
-                this.tweet = this.tweet;
-        }
-        
-        objectify(){
-                
-                
-                return {
-                        "tweet_id":this.tweet_id ,
-                        "source":this.source,
-                        "retweeted": this.retweeted,
-                        "retweeted_count":this.retweeted_count,
-                        "created_at":this.created_at ,
-                        "username":this.username,
-                        "profile_img_url":this.profile_img_url,
-                        "followers":this.followers ,
-                        "user_id":this.user_id,
-                        "tweet":this.tweet,
-                }
-        }
+  constructor(
+    tweet_id,
+    source,
+    retweeted,
+    retweeted_count,
+    created_at,
+    username,
+    user_id,
+    profile_img_url,
+    followers,
+    tweet
+  ) {
+    this.source = source;
+    this.retweeted = retweeted;
+    this.retweeted_count = retweeted_count;
+    this.created_at = created_at;
+    this.username = username;
+    this.profile_img_url = profile_img_url;
+    this.followers = followers;
+    this.user_id = user_id;
+    this.tweet_id = tweet_id;
+    this.tweet = this.tweet;
+  }
 
+  objectify() {
+    return {
+      tweet_id: this.tweet_id,
+      source: this.source,
+      retweeted: this.retweeted,
+      retweeted_count: this.retweeted_count,
+      created_at: this.created_at,
+      username: this.username,
+      profile_img_url: this.profile_img_url,
+      followers: this.followers,
+      user_id: this.user_id,
+      tweet: this.tweet,
+    };
+  }
 }
-
 
 module.exports = Model;
